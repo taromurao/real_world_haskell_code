e85afb070b2c3f75617d5f8b5a5351bf5243b23c
Taro Murao <taro.murao@gmail.com>
2014-07-13 23:06:25 +0200

    ElfMafic.hs created

--- ElfMagic.hs
+++ ElfMagic.hs
@@ -7,4 +7,6 @@ isElfFile path = do
 
 hasElfMagic :: L.ByteString -> Bool
 hasElfMagic content = L.take 4 content == elfMagic
+    -- In the original text, the first bytes are [0x7f, 0x45, 0x4c, 0x46]
+    -- Under OSX, it looks that first four bytes of executable files are as follows:
     where elfMagic = L.pack [207,250,237,254]
