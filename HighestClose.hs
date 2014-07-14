b6522b619427e1b2dc51849279cc9f919ab85065
Taro Murao <taro.murao@gmail.com>
2014-07-14 22:48:23 +0200

    HighestClose.hs 'do' notation

--- HighestClose.hs
+++ HighestClose.hs
@@ -1,18 +1,5 @@
 import qualified Data.ByteString.Lazy.Char8 as L
 
-closing :: L.ByteString -> Maybe Int
-closing = readPrice . (!!4) . L.split ','
-
-readPrice :: L.ByteString -> Maybe Int
-readPrice str =
-    case L.readInt str of
-        Nothing                 -> Nothing
-        Just (dollars, rest)    ->
-            case L.readInt (L.tail rest) of
-                Nothing             -> Nothing
-                Just (cents, more)  ->
-                    Just (dollars * 100 + cents)
-
 highestClose :: L.ByteString -> Maybe Int
 highestClose = maximum . (Nothing:) . map closing . L.lines
 
@@ -20,4 +7,16 @@ highestCloseFrom :: FilePath -> IO()
 highestCloseFrom path = do
     contents <- L.readFile path
     print (highestClose contents)
+
+
+-- private
+
+closing :: L.ByteString -> Maybe Int
+closing = readPrice . (!!4) . L.split ','
+
+readPrice :: L.ByteString -> Maybe Int
+readPrice str = do
+    (dollers, rest) <- L.readInt str
+    (cents, more) <- L.readInt (L.tail rest)
+    return (dollers * 100 + cents)
 
